6970d1eb4994a9f609a4303b9ef8b783fe6d7b02
misterlister <haydenglister@gmail.com>
2025-09-08 12:14:47 -0700

    Added resolution ratios to constants

--- constants.go
+++ constants.go
@@ -4,6 +4,9 @@ const (
 	MaxVideoMemory   = 1 << 30
 	MaxImageMemory   = 1 << 20
 	FilenameByteSize = 32
+	LandscapeRatio   = "16:9"
+	PortraitRatio    = "9:16"
+	OtherRatio       = "other"
 )
 
 var ValidImageTypes = []string{
